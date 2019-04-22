720aa4d8301ceb5f5fd5fa418db746dc2341f2bb
Vic Huang <vic.huang1130x10@gmail.com>
2019-04-22 12:22:07 +0800

    init

---
+++ build/precache-manifest.b6c531267defe99d7864d66f0d6ba6f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ba019138547be908db9",
+    "url": "/static/css/main.f0c40e66.chunk.css"
+  },
+  {
+    "revision": "9ba019138547be908db9",
+    "url": "/static/js/main.9f4a9840.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "8e0b324756ab819caad7",
+    "url": "/static/js/2.50200d29.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dbdb7568923ac70410533138476c3e66",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,25 +1,155 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHaykal } from "@fortawesome/free-solid-svg-icons";
+import logo from "./logo.svg";
+import "./App.css";
+
+const Row = styled.div`
+  display: flex;
+  margin-bottom: 10px;
+  & .message-label {
+    width: 150px;
+    margin-right: 10px;
+    p {
+      text-align: start;
+      font-family: "Oswald", sans-serif;
+
+      span {
+        display: inline-block;
+        background-color: yellowgreen;
+        color: white;
+        padding: 8px 8px;
+        border-radius: 10px;
+      }
+    }
+  }
+  & .message-content {
+    p {
+      margin-top: 28px;
+      font-family: "Arvo", serif;
+    }
+  }
+`;
+
+const Message = ({ type, message, nickname }) => (
+  <Row>
+    <div className="message-label">
+      <p>
+        {type !== "message" ? (
+          <span
+            style={
+              type === "notification"
+                ? { backgroundColor: "blue" }
+                : { backgroundColor: "orange" }
+            }
+          >
+            <FontAwesomeIcon
+              icon={faHaykal}
+              size="sm"
+              style={{ marginRight: "10px" }}
+            />{" "}
+            {` ${type}`}
+          </span>
+        ) : (
+          <span>{nickname}</span>
+        )}
+      </p>
+    </div>
+    <div className="message-content">
+      <p>{message}</p>
+    </div>
+  </Row>
+);
 
 class App extends Component {
+  state = {
+    nickname: "",
+    id: "",
+    message: [],
+    text: ""
+  };
+
+  componentDidMount = () => {
+    this.scrollToBottom();
+    this.ws = new WebSocket("ws://fathomless-shelf-62294.herokuapp.com/");
+    this.ws.onopen = () => {
+      console.log("WebSocket server is connected");
+      // this.ws.send("I'm loggin, ok?");
+    };
+    this.ws.onmessage = e => {
+      var data = JSON.parse(e.data);
+      const { type, id, nickname, message } = data;
+      this.setState({
+        nickname,
+        id,
+        message: [...this.state.message, { type, nickname, message }]
+      });
+      this.scrollToBottom();
+    };
+  };
+
+  onChangeHandler = e => {
+    const text = e.target.value;
+    this.setState({
+      text
+    });
+  };
+
+  onClickHandler = e => {
+    e.preventDefault();
+    const { text } = this.state;
+    if (!text.trim().length) return;
+    this.ws.send(text);
+    this.setState({
+      text: ""
+    });
+    this.scrollToBottom();
+  };
+
+  scrollToBottom = () => {
+    this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+  };
+
   render() {
+    const { message, text } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>The Super chat room</h1>
         </header>
+        <section>
+          <div className="main-section">
+            <h2>Chat Room 1:</h2>
+            <ul className="messages">
+              {message.map((obj, index) => (
+                <Message
+                  type={obj.type}
+                  message={obj.message}
+                  key={index}
+                  nickname={obj.nickname}
+                />
+              ))}
+              <div
+                style={{ float: "left", clear: "both" }}
+                className="dummy"
+                ref={el => {
+                  this.messagesEnd = el;
+                }}
+              />
+            </ul>
+            <form>
+              <input
+                onChange={this.onChangeHandler}
+                type="text"
+                placeholder="Type text to echo in here"
+                value={text}
+              />
+              <button onClick={this.onClickHandler}>Send Message</button>
+            </form>
+          </div>
+        </section>
       </div>
     );
   }
